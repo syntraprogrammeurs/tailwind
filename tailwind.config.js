5c38ec5e25f129e07c2de3a8857ed6120bc6d747
Tom <syntraprogrammeurs@gmail.com>
2024-03-06 19:19:41 +0100

    push

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"], // This now points to HTML and JS files at the project root.
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
